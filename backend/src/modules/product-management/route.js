461ddc010410eb4c5773f6c913f89289eb28d659
Nipuni Perera <nipunithakshilaperera@gmail.com>
2026-07-01 16:39:23 +0530

    chore: stop tracking ignored product management files

--- backend/src/modules/product-management/route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./controller');
-
-router.get('/', controller.getDetails);
-router.get('/inventory', controller.getInventory);
-
-module.exports = router;
