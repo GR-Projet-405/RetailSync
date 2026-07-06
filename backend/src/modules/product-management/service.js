f53ae562450e87cf2aa21d17790992456b851519
Nipuni Perera <nipunithakshilaperera@gmail.com>
2026-07-06 11:02:10 +0530

    feat: Resolve confilct

---
+++ backend/src/modules/product-management/service.js
@@ -0,0 +1,11 @@
+class ProductPageService {
+  async fetchDetails() {
+    // Skeletons to be populated by development teams
+    return {
+      module: 'Product Management',
+      status: 'Under Development'
+    };
+  }
+}
+
+module.exports = new ProductPageService();
